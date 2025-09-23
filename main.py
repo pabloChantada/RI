15784a8f04eff6f1bd90d9194e308222470307da
pabloChantada <147641118+pabloChantada@users.noreply.github.com>
2025-09-24 00:26:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import gymnasium as gym
+from gym_env import CylinderEnv
+
+env = CylinderEnv()
+
+# Reset environment to start a new episode
+observation, info = env.reset()
+# observation: what the agent can "see" - cart position, velocity, pole angle, etc.
+# info: extra debugging information (usually not needed for basic learning)
+
+print(f"Starting observation: {observation}")
+
+episode_over = False
+total_reward = 0
+
+while not episode_over:
+    action = env.action_space.sample()  # Random action for now - real agents will be smarter!
+
+    # Take the action and see what happens
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    total_reward += reward
+    episode_over = terminated or truncated
+
+print(f"Episode finished! Total reward: {total_reward}")
+env.close()
