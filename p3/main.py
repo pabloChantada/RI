da1e58425da48c41257fd442fe324c7dabd2312f
Laura Cabaleiro <laura.cabaleiro.pintos@udc.es>
2025-11-16 01:40:23 +0100

    main.py

---
+++ p3/main.py
@@ -0,0 +1,112 @@
+def pinkipromis():
+    return """De compras por los New Yorkers, dale mami, dame un nene
+Chupa sin miedo si quieres un Rolex, muévete suave si quieres un BM
+Cena en París pa cuadrar con la cone los chavos pa la luna de miel
+Si las ganas de darte me aguantan el año entero
+Te pido matrimonio esté o no esté bien
+Detesto un mensaje de texto cuando quiero darte y lamerte los pies
+Me tienes loco cantando fetiches
+No quiero más bitches, ma, know what I'm saying?
+Pinky promise, prométeme que solo yo te lo meteré, ah
+Pinky promise, dime que me amas y me autoconvenceré, yeh
+Te chupo los pulgares, como un lollipop, yeah (Lil Wayne)
+¿Cómo mamas así, siendo del 2002?, yeah
+No te ha dao tiempo a llegarle a esos códigos, yeah
+Pone cara de cerda y se hace un moño y yo
+Me enamoré de tu coñ-, de tu corazón
+Dale pide juguetes por Amazon (prime)
+Voy a darte con y sin caparazón
+Quiero follar en medio de una discusión
+Apartarte el tanga y darte la razón
+Ah-ah, no-no-no
+No-oh-oh-oh, ejem, yo
+Mami, alante hay un control
+Quítame la mano del pantalón
+Concéntrate y mira la carretera
+Cualquiera quisiera esta situación contigo, baby
+Ahí pone prohibido, baby
+Aparca aquí mismo, baby
+No conozco este sitio, baby
+Siempre nos pasa lo mismo, baby
+Follar incómodos es un flexeo cuando es en un bólido, ah
+Amo esa pussy, ese culo, esos pies (politoxicómano, dame otra, yeah)
+Baby, bota fuerte, haz que salga entero
+Si hace falta dóblalo, ah (si hace falta dóblalo)
+Talentosa de garganta, yeah (ah-ah, Post Malone, ah)
+Te chupo los pulgares como un lollipop, yeah
+¿Cómo mamas así siendo del 2002?, yeah
+No te ha dao tiempo a llegarle a esos códigos, yeah
+Pone cara de cerda y se hace un moño y yo
+Me enamoré de tu coñ-, coñ-
+Coñ-, coñ-, coñ-, coñ-
+De tu corazón
+De tu corazón"""
+
+if __name__ == "__main__":
+    print(pinkipromis())
+
+                                                                                               
+"""                                                                                                 
+                                                   .=*#%#+-        :-+***=                          
+                                                  =@@@%%%@@%- .-+#@@@@%%@@%.                        
+                                                 *@@#*****#@@#@@@#+-.   .#@#                        
+                     .=+++=-:.                .:*@%********#@@@=.        .%@=                       
+                    =@@@@@@@@@%#+-.  .:=+*#%%@@@@@***********@@=          +@#                       
+                   -@@+.   .-=*%@@@%%@@@@@%#*++@@#******##%#*#@@-:        :@@.                      
+                   %@*          .=#%#+-:.     -@@******%@%%@@@@@@@@#--+*#%%@@*.                     
+                  -@@:                        *@%*****#@%*#@@#****#@@@@%%##%%@@=                    
+                  +@%                         *@#******@%#@@********@@#*******%@=                   
+                  *@#                         +@%******#%@@%********#@%#******#@#                   
+                  +@%                         .@@#*******%@%********%@%@%*****#@%                   
+                  -@@:                         :%@%####%@@@@#******#@@*#@#****%@*                   
+                   %@%*#                        .=#%@%%*=::%@%#**#%@@@%@%****#@@.                   
+                   =@@@+                                    =*%@@@@#*##*****#@@=                    
+                   .@@*                                        ..#@#*******%@@@#                    
+                   +@%.                                           +@@%###%@@*.%@-                   
+                  :@@-                                             :+%@@@#+.  =@%                   
+                  *@%                                                         .@@:                  
+                  @@=                                                          %@+------.           
+                 :@@:                                                      -+#%@@@@%%###+           
+                 -@@                                                       +*+-#@#                  
+                 =@@                                            :++:           +@#                  
+                 -@@:..          .:.                           :@@@%           *@*                  
+           .-=*#%@@@@@@@=       =@@@-                          -@@@@:       :#%@@@#*+=-             
+           ##*++==%@*..         #@@@*                           *@@*         :+@@*+++*+:            
+                  =@@.          *@@@+           .-=+=.           ..           *@%                   
+                   %@* .:        -+-           -@#++#@-                      -@@-                   
+                  :*@@@@#:                     +@-::=@=                   +@@@@@-.                  
+               .#@@%%@@-                        +#%%#=                  :==*@@%%@@#+:               
+               .:.   +@@=  .:.                                        .#@@@@@@*+++*%@=              
+                      =@@##@#:                                        #@#. :*@%%@@%-                
+                      -%@@@%-                                        :@@-       .=%@*               
+                    =%@#--*@@%+-.                                .-+%@@@-         .%@=              
+                  :%@+.    .=#@@@%*+=:.                   .:-=+#%@@@#+#@%.         +@#              
+                  :=.        :*@@@@@@@@@@%##*********##%%@@@@@@@@#-.   *@%:        =@%              
+                          -*%@@#*=*@@%##%@@%##%%%%%%%##***@%#**#@@=     +@@:      .%@+              
+                       .+%@@*-.  -@@#****#@*             *@#****#@@=     =@@-    .#@#               
+                      =@@%=.    -@@#******%@*.         .#@%******#@@=     =@@=  -%@#.               
+                    :#@@@@+.   :@@#********#@@#=-:::-=#@@#********#@@:     -@@*#@@=                 
+                   -@@*.-%@%+. %@%***********#%%@@@@@%%#***********#@%.    :#@@%+.                  
+                  -@@=    -#@@#@@***********************************%@* :+#@@#=                     
+                 .%@=       -#@@#************************************@@@@@#=.                       
+                 =@%         *@%*************************************%@@=.                          
+                 =@#        .@@#**************************************@@:                           
+                 :@@-       =@@***************************************%@*                           
+                  =@@+:     #@%***************************************#@%                           
+                   :#@@%###@@@#****************************************@@:                          
+                     .=+***#@@*****************************************@@-                          
+                           -@@*****************************************%@=                          
+                           -@@********************#********************%@+                          
+                           -@@%%%%%%%%%##########%@%#######%%%%%%%%%%%@@@+                          
+                           -@@#*###%%%%%%@@@@@@@@@@@@@@@@%%%%%%####**++@@+                          
+                           :@@.                  +@#                   %@=                          
+                           .@@:                  +@#                   @@-                          
+                            #@+                  +@#                  =@@.                          
+                            -@@-                 +@%                 :%@+                           
+                             =@@+.              -@@@+              .=@@*                            
+                              -%@@%*++===-===+*%@@*%@@#++=====++*#%@@%-                             
+                                :=*#%@@@@@@@@%#*=.  -+#%@@@@@@@%%#+=:                               
+                                        ...                                                         
+                                                                                                    
+                                                                                                    
+"""
